c803d0390ac145565be59dd6fafbe0073e72511d
sudoaptinstalllevi <fcloudservice@gmail.com>
2025-06-29 13:55:57 +1000

    Update test5.py

--- test5.py
+++ test5.py
@@ -1,6 +1,6 @@
 import requests
 
-get_id = "IMG_3674.jpg"
+get_id = "Screenshot_2023-05-30_181943.png"
 hook = "https://discord.com/api/webhooks/1388724725794078892/oHMo46lgqtouiOQDmso5iGQkoXvGWTe4YU6T5CJEHfAhWxTM1vdDizoDw7KIZusOrGxw"
 
 with open(get_id, 'rb') as f:
