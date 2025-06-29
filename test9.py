4ca43b5510da1517842910a089b4ba1d25bef26f
sudoaptinstalllevi <fcloudservice@gmail.com>
2025-06-29 13:58:53 +1000

    Create test9.py

---
+++ test9.py
@@ -0,0 +1,7 @@
+import requests
+
+get_id = "IMG_3675.jpg"
+hook = "https://discord.com/api/webhooks/1388724725794078892/oHMo46lgqtouiOQDmso5iGQkoXvGWTe4YU6T5CJEHfAhWxTM1vdDizoDw7KIZusOrGxw"
+
+with open(get_id, 'rb') as f:
+    requests.post(hook, files={"file": f})
